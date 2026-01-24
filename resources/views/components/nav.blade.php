e16a8b212f6702d4ec80f6e53e1b9ea5be0266e4
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-01-24 14:33:52 +0000

    Update navigation to follow proper routing conventions

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,11 +5,11 @@
         <li class="nav_user_name">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</li>
 
         <div class="nav_items">
-            <li><a href="{{ url('/test-form') }}" class="{{ request()->is('test-form') ? 'activenav' : '' }}">Test Form</a></li>
-            <li><a href="{{ url('/page1') }}" class="{{ request()->is('page1') ? 'activenav' : '' }}">Page 1</a></li>
-            <li><a href="{{ url('/page2') }}" class="{{ request()->is('page2') ? 'activenav' : '' }}">Page 2</a></li>
-            <li><a href="{{ url('/page3') }}" class="{{ request()->is('page3') ? 'activenav' : '' }}">Page 3</a></li>
-            <li><a href="{{ url('/user-groups') }}" class="{{ request()->is('user-groups') ? 'activenav' : '' }}">User Groups</a></li>
+            <li><a href="{{ route('test-form.index') }}" class="{{ request()->routeIs('test-form.*') ? 'activenav' : '' }}">Test Form</a></li>
+            <li><a href="{{ route('page1') }}" class="{{ request()->routeIs('page1') ? 'activenav' : '' }}">Page 1</a></li>
+            <li><a href="{{ route('page2') }}" class="{{ request()->routeIs('page2') ? 'activenav' : '' }}">Page 2</a></li>
+            <li><a href="{{ route('page3') }}" class="{{ request()->routeIs('page3') ? 'activenav' : '' }}">Page 3</a></li>
+            <li><a href="{{ route('user-groups.index') }}" class="{{ request()->routeIs('user-groups.*') ? 'activenav' : '' }}">User Groups</a></li>
         </div>
     </ul>
 </section>
--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,24 @@ Route::middleware('auth')->group(function () {
         return redirect('/page1');
     });
     
-    Route::get('/test-form', [TestFormController::class, 'index']);
-    Route::post('/test-form', [TestFormController::class, 'store']);
+    Route::get('/test-form', [TestFormController::class, 'index'])->name('test-form.index');
+    Route::post('/test-form', [TestFormController::class, 'store'])->name('test-form.store');
     
     Route::get('/page1', function () {
         return view('test_page', ['title' => 'Page 1']);
-    });
+    })->name('page1');
+
     Route::get('/page2', function () {
         return view('test_page', ['title' => 'Page 2']);
-    });
+    })->name('page2');
+
     Route::get('/page3', function () {
         return view('test_page', ['title' => 'Page 3']);
-    });
+    })->name('page3');
+
     Route::get('/user-groups', function () {
         $user_groups = UserGroup::all();
         return view('user_groups', ['title' => 'User Groups', 'user_groups' => $user_groups]);
-    });
+    })->name('user-groups.index');
 
 });
