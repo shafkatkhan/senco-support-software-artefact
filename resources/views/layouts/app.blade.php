ff9f368533edf5280cecfc19253e21e0d7519ccf
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-01-24 15:44:56 +0000

    Implement basic error reporting
    - also minor indentation fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,5 +19,11 @@
 <script src="{{ asset('js/jquery.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('js/script.js') }}"></script>
+
+@if($errors->any())
+    <script>
+        alert("{{ $errors->first() }}");
+    </script>
+@endif
 </body>
 </html>
