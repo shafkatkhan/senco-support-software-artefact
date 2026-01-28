c4e811c648bdc0984751270910187258af80fe27
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-01-28 22:52:52 +0000

    Translate top_nav

--- resources/views/components/top_nav.blade.php
+++ resources/views/components/top_nav.blade.php
@@ -3,21 +3,21 @@
     <ul>
         @if(request()->is('pupil-*'))
             <li>
-                <a href="{{ route('pupils.show', $pupil->id) }}" class="{{ request()->routeIs('pupils.show') ? 'activenav' : '' }}">Summary</a>
+                <a href="{{ route('pupils.show', $pupil->id) }}" class="{{ request()->routeIs('pupils.show') ? 'activenav' : '' }}">{{ __('Summary') }}</a>
             </li>
             <li>
-                <a href="{{ route('pupils.medications', $pupil->id) }}" class="{{ request()->routeIs('pupils.medications') ? 'activenav' : '' }}">Medications</a>
+                <a href="{{ route('pupils.medications', $pupil->id) }}" class="{{ request()->routeIs('pupils.medications') ? 'activenav' : '' }}">{{ __('Medications') }}</a>
             </li>
             <li>
-                <a href="{{ route('pupils.diagnoses', $pupil->id) }}" class="{{ request()->routeIs('pupils.diagnoses') ? 'activenav' : '' }}">Diagnoses</a>
+                <a href="{{ route('pupils.diagnoses', $pupil->id) }}" class="{{ request()->routeIs('pupils.diagnoses') ? 'activenav' : '' }}">{{ __('Diagnoses') }}</a>
             </li>
             <li>
-                <a href="{{ route('pupils.family_members', $pupil->id) }}" class="{{ request()->routeIs('pupils.family_members') ? 'activenav' : '' }}">Family Members</a>
+                <a href="{{ route('pupils.family_members', $pupil->id) }}" class="{{ request()->routeIs('pupils.family_members') ? 'activenav' : '' }}">{{ __('Family Members') }}</a>
             </li>
         @endif
         <li class="logout">
             <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Logout <i class="fas fa-power-off"></i>
+                {{ __('Logout') }} <i class="fas fa-power-off"></i>
             </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
