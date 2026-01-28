fec61bd813a063a0318352f7bd7648936f55aafb
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-01-28 22:13:57 +0000

    Add multilingual support for nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,12 +5,12 @@
         <li class="nav_user_name">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</li>
 
         <div class="nav_items">
-            <li><a href="{{ route('test-form.index') }}" class="{{ request()->routeIs('test-form.*') ? 'activenav' : '' }}">Test Form</a></li>
-            <li><a href="{{ route('page1') }}" class="{{ request()->routeIs('page1') ? 'activenav' : '' }}">Page 1</a></li>
-            <li><a href="{{ route('pupils.index') }}" class="{{ request()->routeIs('pupils.*') ? 'activenav' : '' }}">SEND Pupils</a></li>
-            <li><a href="{{ route('accommodations.index') }}" class="{{ request()->routeIs('accommodations.*') ? 'activenav' : '' }}">Accommodations</a></li>
-            <li><a href="{{ route('user-groups.index') }}" class="{{ request()->routeIs('user-groups.*') ? 'activenav' : '' }}">User Groups</a></li>
-            <li><a href="{{ route('users.index') }}" class="{{ request()->routeIs('users.*') ? 'activenav' : '' }}">Users</a></li>
+            <li><a href="{{ route('test-form.index') }}" class="{{ request()->routeIs('test-form.*') ? 'activenav' : '' }}">{{ __('Test Form') }}</a></li>
+            <li><a href="{{ route('page1') }}" class="{{ request()->routeIs('page1') ? 'activenav' : '' }}">{{ __('Page 1') }}</a></li>
+            <li><a href="{{ route('pupils.index') }}" class="{{ request()->routeIs('pupils.*') ? 'activenav' : '' }}">{{ __('SEND Pupils') }}</a></li>
+            <li><a href="{{ route('accommodations.index') }}" class="{{ request()->routeIs('accommodations.*') ? 'activenav' : '' }}">{{ __('Accommodations') }}</a></li>
+            <li><a href="{{ route('user-groups.index') }}" class="{{ request()->routeIs('user-groups.*') ? 'activenav' : '' }}">{{ __('User Groups') }}</a></li>
+            <li><a href="{{ route('users.index') }}" class="{{ request()->routeIs('users.*') ? 'activenav' : '' }}">{{ __('Users') }}</a></li>
         </div>
     </ul>
 </section>
