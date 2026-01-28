11aade446f8792251a1edade2d2e8d61598dabad
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-01-28 21:48:05 +0000

    Minor

--- resources/views/install.blade.php
+++ resources/views/install.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>MySencoSupportSoftware - Installation</title>
+    <title>MySencoSupportSoftware | Installation</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
