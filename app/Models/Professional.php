05888f7aee6894fe95a8b78ca63ec7680ad25803
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-01 12:44:08 +0000

    Create professionals table

---
+++ app/Models/Professional.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Professional extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'first_name',
+        'last_name',
+        'role',
+        'agency',
+        'phone',
+        'email',
+    ];
+}
