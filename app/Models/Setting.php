5350f7304824a5953f8793738547bb7062c2a173
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-08 22:45:34 +0000

    Create settings table

---
+++ app/Models/Setting.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    protected $fillable = [
+        'key',
+        'value',
+    ];
+
+    /**
+     * Get a settings value by key, with an optional default value.
+     */
+    public static function get(string $key, mixed $default = null): mixed
+    {
+        return static::where('key', $key)->value('value') ?? $default;
+    }
+
+    /**
+     * Set a settings value by key (create or update).
+     */
+    public static function set(string $key, mixed $value): void
+    {
+        static::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+    }
+}
