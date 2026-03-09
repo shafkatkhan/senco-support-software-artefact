e1b3349a39506dc8c61280c7fadfff2df05d37fa
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-09 17:19:45 +0000

    Fix AppServiceProvider crashing pre-installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,17 +22,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // override mail config
-        if (Schema::hasTable('settings') && Setting::get('mail_host')) { // only run config overrides if the settings table actually exists and has a mail host set
-            config([
-                'mail.default' => 'smtp',
-                "mail.mailers.smtp.host" => Setting::get('mail_host'),
-                "mail.mailers.smtp.port" => Setting::get('mail_port'),
-                "mail.mailers.smtp.username" => Setting::get('mail_username'),
-                "mail.mailers.smtp.password" => Setting::get('mail_password'),
-                "mail.mailers.smtp.encryption" => Setting::get('mail_encryption'),
-                'mail.from.address' => Setting::get('mail_from_address'),
-                'mail.from.name' => Setting::get('mail_from_name'),
-            ]);
+        try {
+            if (Schema::hasTable('settings') && Setting::get('mail_host')) { // only run config overrides if the settings table actually exists and has a mail host set
+                config([
+                    'mail.default' => 'smtp',
+                    "mail.mailers.smtp.host" => Setting::get('mail_host'),
+                    "mail.mailers.smtp.port" => Setting::get('mail_port'),
+                    "mail.mailers.smtp.username" => Setting::get('mail_username'),
+                    "mail.mailers.smtp.password" => Setting::get('mail_password'),
+                    "mail.mailers.smtp.encryption" => Setting::get('mail_encryption'),
+                    'mail.from.address' => Setting::get('mail_from_address'),
+                    'mail.from.name' => Setting::get('mail_from_name'),
+                ]);
+            }
+        } catch (\Exception $e) {
+            // database might not exist yet, or connection failed; safely ignore to allow installation
         }
     }
 }
