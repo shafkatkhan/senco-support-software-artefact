53aa45dd1ae0e21e30d3c1e36eb39e4e860522c2
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-10 20:16:29 +0000

    Update FamilyMemberFactory for new columns

--- database/factories/FamilyMemberFactory.php
+++ database/factories/FamilyMemberFactory.php
@@ -25,6 +25,18 @@ class FamilyMemberFactory extends Factory
             'dob' => fake()->dateTimeBetween('-50 years', '-2 years')->format('Y-m-d'),
             'relation' => fake()->randomElement($relations),
             'pupil_id' => Pupil::factory(),
+            'phone' => fake()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'address_line_1' => fake()->streetAddress(),
+            'address_line_2' => fake()->optional()->secondaryAddress(),
+            'locality' => fake()->city(),
+            'postcode' => fake()->postcode(),
+            'country' => 'United Kingdom',
+            'marital_status' => $this->faker->boolean(60) ? fake()->randomElement(['Married', 'Single', 'Divorced', 'Widowed', 'Separated']) : null,
+            'highest_education' => $this->faker->boolean(60) ? fake()->randomElement(['GCSEs', 'A Levels', 'Bachelor\'s Degree', 'Master\'s Degree', 'PhD']) : null,
+            'financial_status' => $this->faker->boolean(60) ? fake()->randomElement(['Stable', 'Struggling', 'Comfortable']) : null,
+            'occupation' => $this->faker->boolean(40) ? fake()->jobTitle() : null,
+            'state_support' => $this->faker->boolean(60) ? fake()->randomElement(['Universal Credit', 'Child Benefit', 'Housing Benefit']) : null,
         ];
     }
 }
