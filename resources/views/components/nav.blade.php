7658f009d28047c8029a02bacf686a1baa5bd0d4
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-10 14:06:59 +0000

    Conditionally display side navigation links based on user permissions

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,23 +7,51 @@
         <div class="nav_items @if(auth()->user()->isMfaPending()) nav_disabled @endif">
             <li><a href="{{ route('test-form.index') }}" class="{{ request()->routeIs('test-form.*') ? 'activenav' : '' }}">{{ __('Test Form') }}</a></li>
             <li><a href="{{ route('page1') }}" class="{{ request()->routeIs('page1') ? 'activenav' : '' }}">{{ __('Page 1') }}</a></li>
-            <li><a href="{{ route('pupils.index') }}" class="{{ request()->routeIs('pupils.*') ? 'activenav' : '' }}">{{ __('SEND Pupils') }}</a></li>
-            <li><a href="{{ route('accommodations.index') }}" class="{{ request()->routeIs('accommodations.*') ? 'activenav' : '' }}">{{ __('Accommodations') }}</a></li>
-            <li><a href="{{ route('majors.index') }}" class="{{ request()->routeIs('majors.*') ? 'activenav' : '' }}">{{ __('Majors') }}</a></li>
-            <li><a href="{{ route('proficiencies.index') }}" class="{{ request()->routeIs('proficiencies.*') ? 'activenav' : '' }}">{{ __('Proficiencies') }}</a></li>
-            <li><a href="{{ route('subjects.index') }}" class="{{ request()->routeIs('subjects.*') ? 'activenav' : '' }}">{{ __('Subjects') }}</a></li>
-            <li><a href="{{ route('record-types.index') }}" class="{{ request()->routeIs('record-types.*') ? 'activenav' : '' }}">{{ __('Record Types') }}</a></li>
-            <li><a href="{{ route('meeting-types.index') }}" class="{{ request()->routeIs('meeting-types.*') ? 'activenav' : '' }}">{{ __('Meeting Types') }}</a></li>
-            <li><a href="{{ route('professionals.index') }}" class="{{ request()->routeIs('professionals.*') ? 'activenav' : '' }}">{{ __('Professionals') }}</a></li>
-            <li><a href="{{ route('user-groups.index') }}" class="{{ request()->routeIs('user-groups.*') ? 'activenav' : '' }}">{{ __('User Groups') }}</a></li>
-            <li><a href="{{ route('users.index') }}" class="{{ request()->routeIs('users.*') ? 'activenav' : '' }}">{{ __('Users') }}</a></li>
+            @can('view-pupils')
+                <li><a href="{{ route('pupils.index') }}" class="{{ request()->routeIs('pupils.*') ? 'activenav' : '' }}">{{ __('SEND Pupils') }}</a></li>
+            @endcan
+            @can('view-accommodations')
+                <li><a href="{{ route('accommodations.index') }}" class="{{ request()->routeIs('accommodations.*') ? 'activenav' : '' }}">{{ __('Accommodations') }}</a></li>
+            @endcan
+            @can('view-majors')
+                <li><a href="{{ route('majors.index') }}" class="{{ request()->routeIs('majors.*') ? 'activenav' : '' }}">{{ __('Majors') }}</a></li>
+            @endcan
+            @can('view-proficiencies')
+                <li><a href="{{ route('proficiencies.index') }}" class="{{ request()->routeIs('proficiencies.*') ? 'activenav' : '' }}">{{ __('Proficiencies') }}</a></li>
+            @endcan
+            @can('view-subjects')
+                <li><a href="{{ route('subjects.index') }}" class="{{ request()->routeIs('subjects.*') ? 'activenav' : '' }}">{{ __('Subjects') }}</a></li>
+            @endcan
+            @can('view-record-types')
+                <li><a href="{{ route('record-types.index') }}" class="{{ request()->routeIs('record-types.*') ? 'activenav' : '' }}">{{ __('Record Types') }}</a></li>
+            @endcan
+            @can('view-meeting-types')
+                <li><a href="{{ route('meeting-types.index') }}" class="{{ request()->routeIs('meeting-types.*') ? 'activenav' : '' }}">{{ __('Meeting Types') }}</a></li>
+            @endcan
+            @can('view-professionals')
+                <li><a href="{{ route('professionals.index') }}" class="{{ request()->routeIs('professionals.*') ? 'activenav' : '' }}">{{ __('Professionals') }}</a></li>
+            @endcan
+            @can('view-user-groups')
+                <li><a href="{{ route('user-groups.index') }}" class="{{ request()->routeIs('user-groups.*') ? 'activenav' : '' }}">{{ __('User Groups') }}</a></li>
+            @endcan
+            @can('view-users')
+                <li><a href="{{ route('users.index') }}" class="{{ request()->routeIs('users.*') ? 'activenav' : '' }}">{{ __('Users') }}</a></li>
+            @endcan
             <li class="dropdown_nav_btn"><a href="#">{{ __('System Settings') }} <i class="fas fa-chevron-left"></i></a></li>
             <div class="dropdown_items">
-                <li class="sub_nav_item"><a href="{{ route('email-settings.index') }}" class="{{ request()->routeIs('email-settings.*') ? 'activenav' : '' }}">{{ __('Email Settings') }}</a></li>
-                <li class="sub_nav_item"><a href="{{ route('mfa-settings.index') }}" class="{{ request()->routeIs('mfa-settings.*') ? 'activenav' : '' }}">{{ __('MFA Settings') }}</a></li>
-                <li class="sub_nav_item"><a href="{{ route('mfa-setup.index') }}" class="{{ request()->routeIs('mfa-setup.*') ? 'activenav' : '' }}">{{ __('MFA Setup') }}</a></li>
-                <li class="sub_nav_item"><a href="{{ route('permissions.index') }}" class="{{ request()->routeIs('permissions.*') ? 'activenav' : '' }}">{{ __('Permissions') }}</a></li>
-                <li class="sub_nav_item"><a href="{{ route('backups.index') }}" class="{{ request()->routeIs('backups.*') ? 'activenav' : '' }}">{{ __('System Backups') }}</a></li>
+                @can('manage-email-settings')
+                    <li class="sub_nav_item"><a href="{{ route('email-settings.index') }}" class="{{ request()->routeIs('email-settings.*') ? 'activenav' : '' }}">{{ __('Email Settings') }}</a></li>
+                @endcan
+                @can('manage-mfa-settings')
+                    <li class="sub_nav_item"><a href="{{ route('mfa-settings.index') }}" class="{{ request()->routeIs('mfa-settings.*') ? 'activenav' : '' }}">{{ __('MFA Settings') }}</a></li>
+                @endcan
+                    <li class="sub_nav_item"><a href="{{ route('mfa-setup.index') }}" class="{{ request()->routeIs('mfa-setup.*') ? 'activenav' : '' }}">{{ __('MFA Setup') }}</a></li>
+                @can('manage-permissions')
+                    <li class="sub_nav_item"><a href="{{ route('permissions.index') }}" class="{{ request()->routeIs('permissions.*') ? 'activenav' : '' }}">{{ __('Permissions') }}</a></li>
+                @endcan
+                @can('view-download-backups')
+                    <li class="sub_nav_item"><a href="{{ route('backups.index') }}" class="{{ request()->routeIs('backups.*') ? 'activenav' : '' }}">{{ __('System Backups') }}</a></li>
+                @endcan
             </div>
         </div>
     </ul>
