90943aa84c1d5d7e3352bf1059929c77628d274e
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-10 14:41:30 +0000

    Enforce Meeting Type Management user permissions
    - also add empty table message

--- app/Http/Controllers/MeetingTypeController.php
+++ app/Http/Controllers/MeetingTypeController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\MeetingType;
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Gate;
 
 class MeetingTypeController extends Controller
 {
     public function index()
     {
+        Gate::authorize('view-meeting-types');
+
         $meeting_types = MeetingType::all();
         $title = "Meeting Types";
         return view('meeting_types', compact('meeting_types', 'title'));
@@ -17,6 +20,8 @@ class MeetingTypeController extends Controller
 
     public function store(Request $request)
     {
+        Gate::authorize('create-meeting-types');
+
         MeetingType::create($request->validate([
             'name' => 'required|unique:meeting_types,name|max:255',
             'description' => 'nullable|string',
@@ -27,6 +32,8 @@ class MeetingTypeController extends Controller
 
     public function update(Request $request, MeetingType $meeting_type)
     {
+        Gate::authorize('edit-meeting-types');
+
         $meeting_type->update($request->validate([
             'name' => 'required|max:255|unique:meeting_types,name,' . $meeting_type->id,
             'description' => 'nullable|string',
@@ -37,6 +44,8 @@ class MeetingTypeController extends Controller
 
     public function destroy(MeetingType $meeting_type)
     {
+        Gate::authorize('delete-meeting-types');
+        
         try {
             $meeting_type->delete();
             return back()->with('success', 'Meeting Type Deleted Successfully!');
--- resources/views/meeting_types.blade.php
+++ resources/views/meeting_types.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <section id="content">
         <div class="content_top_buttons">
+            @can('create-meeting-types')
             <button type="button" class="new_button" data-bs-toggle="modal" data-bs-target="#new">
                 Create Meeting Type
             </button>
+            @endcan
         </div>
         <div class="table_wrap">
             <table class="table sen_table-striped">
@@ -14,26 +16,39 @@
                         <th scope="col">#</th>
                         <th scope="col">Name</th>
                         <th scope="col">Description</th>
+                        @canany(['edit-meeting-types', 'delete-meeting-types'])
                         <th scope="col">Actions</th>
+                        @endcanany
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($meeting_types as $meeting_type)
+                    @forelse($meeting_types as $meeting_type)
                         <tr>
                             <th scope="row">{{ $loop->iteration }}</th>
                             <td>{{ $meeting_type->name }}</td>
                             <td>{{ $meeting_type->description }}</td>
+                            @canany(['edit-meeting-types', 'delete-meeting-types'])
                             <td class="icon_wrap">
+                                @can('edit-meeting-types')
                                 <button class="icon edit_icon" data-bs-toggle="modal" data-bs-target="#edit" data-url="{{ route('meeting-types.update', $meeting_type->id) }}" data-name="{{ $meeting_type->name }}" data-description="{{ $meeting_type->description }}"><i class="fa fa-edit"></i></button>
+                                @endcan
+                                @can('delete-meeting-types')
                                 <button class="icon delete_icon" data-bs-toggle="modal" data-bs-target="#delete" data-url="{{ route('meeting-types.destroy', $meeting_type->id) }}" data-name="{{ $meeting_type->name }}"><i class="fa fa-trash-alt"></i></button>
+                                @endcan
                             </td>
+                            @endcanany
                         </tr>
-                    @endforeach
+                    @empty
+                        <tr>
+                            <td colspan="{{ auth()->user()->canAny(['edit-meeting-types', 'delete-meeting-types']) ? '4' : '3' }}" class="empty_table_message">No meeting types found.</td>
+                        </tr>
+                    @endforelse
                 </tbody>
             </table>
         </div>
     </section>
 
+    @can('create-meeting-types')
     <div class="modal fade" id="new" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -60,7 +75,9 @@
             </div>
         </div>
     </div>
+    @endcan
 
+    @can('edit-meeting-types')
     <div class="modal fade" id="edit" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
@@ -88,8 +105,11 @@
             </div>
         </div>
     </div>
+    @endcan
 
+    @can('delete-meeting-types')
     @include('components.delete_modal', ['type' => 'Meeting Type'])
+    @endcan
 @endsection
 
 @push('scripts')
