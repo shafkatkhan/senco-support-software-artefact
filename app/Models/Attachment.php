ff569be0520ccece813dd1ddeddd31aea79b76d2
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-12 22:52:54 +0000

    Create attachments table

---
+++ app/Models/Attachment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attachment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'attachable_type',
+        'attachable_id',
+        'filename',
+        'file_path',
+        'mime_type',
+        'size_bytes',
+    ];
+    
+    public function attachable()
+    {
+        return $this->morphTo();
+    }
+}
