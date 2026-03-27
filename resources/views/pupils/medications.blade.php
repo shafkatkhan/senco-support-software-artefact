aa318548cc315b933f47f9fcae22d0ce0531a1c8
shafkatkhan <sajid.khan07@hotmail.co.uk>
2026-03-27 14:29:37 +0000

    Refactor pupil page top header into separate blade component

--- resources/views/pupils/medications.blade.php
+++ resources/views/pupils/medications.blade.php
@@ -2,40 +2,7 @@
 
 @section('content')
     <section id="content">
-        <div class="content_top_buttons justify-content-between">
-           <div class="section_title">
-                <a href="{{ route('pupils.index') }}" class="previous_icon"><i class="fas {{ is_rtl() ? 'fa-arrow-circle-right' : 'fa-arrow-circle-left' }}"></i></a> {{ __('Return back to pupils') }}
-            </div>
-            <div style="display: flex; gap: 10px; justify-content: flex-end;">
-                @can('export-pupil-data')
-                <div class="dropdown">
-                    <button class="top_button export_button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fas fa-file-export"></i> {{ __('Export') }}
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li>
-                            <a class="dropdown-item" href="{{ route('pupils.medications.export', ['pupil' => $pupil->id, 'format' => 'excel']) }}">
-                                <i class="fas fa-file-excel text-success"></i> {{ __('Export to Excel') }}
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('pupils.medications.export', ['pupil' => $pupil->id, 'format' => 'csv']) }}">
-                                <i class="fas fa-file-csv text-muted"></i> {{ __('Export to CSV') }}
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                @endcan
-                <button type="button" class="top_button toggle_button" id="toggleViewBtn">
-                    {{ __('Toggle Card View') }}
-                </button>
-                @can('create-medications')
-                <button type="button" class="top_button" data-bs-toggle="modal" data-bs-target="#new">
-                    {{ __('Add New Medication') }}
-                </button> 
-                @endcan
-            </div>            
-        </div>
+        @include('components.pupil_page_top_header', ['pupil' => $pupil, 'new_button_text' => __('Add New Medication'), 'route_name' => 'medications'])
 
         <div id="toggleViewGrid" class="sen_cards" style="display: none;">
             @forelse($pupil->medications as $medication)
